f6d402e1358164e1326b0bfdf0a250a291729ad2
Bernardo Breder <bbreder@tecgraf.puc-rio.br>
2017-05-08 08:39:51 -0300

    .

--- bin/make.bat
+++ bin/make.bat
@@ -4,7 +4,7 @@ echo Clearing...
 rm -f dist\flow.exe
 mkdir dist
 echo Compiling...
-gcc -I inc src\*.c -o dist\flow.exe
+gcc -I inc src\*.c -lUser32 -o dist\flow.exe
 echo Executing...
-dist\flow
+dist\flow src\main.c
 echo Finished
