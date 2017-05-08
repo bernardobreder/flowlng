9d976bf7db2f2d66cfb09fef25d0e02b33ba6798
Bernardo Breder <bbreder@tecgraf.puc-rio.br>
2017-05-08 13:54:31 -0300

    .

--- src/windows/watch.c
+++ src/windows/watch.c
@@ -13,13 +13,12 @@
 #include <time.h>
 #include <utime.h>
 
-#include <tchar.h> 
+#include <tchar.h>
 #include <stdio.h>
 #include <strsafe.h>
 
 #include <wincrypt.h>
 #pragma comment(lib, "User32.lib")
-
 
 BOOL GetLastWriteTime(HANDLE hFile, LPTSTR lpszString, DWORD dwSize)
 {
@@ -36,40 +35,42 @@ BOOL GetLastWriteTime(HANDLE hFile, LPTSTR lpszString, DWORD dwSize)
     SystemTimeToTzSpecificLocalTime(NULL, &stUTC, &stLocal);
 
     // Build a string showing the date and time.
-    dwRet = StringCchPrintf(lpszString, dwSize, 
-        TEXT("%02d/%02d/%d  %02d:%02d:%02d:%03d"),
-        stLocal.wMonth, stLocal.wDay, stLocal.wYear,
-        stLocal.wHour, stLocal.wMinute, stLocal.wSecond, stLocal.wMilliseconds);
+    dwRet = StringCchPrintf(lpszString, dwSize,
+                            TEXT("%02d/%02d/%d  %02d:%02d:%02d:%03d"),
+                            stLocal.wMonth, stLocal.wDay, stLocal.wYear,
+                            stLocal.wHour, stLocal.wMinute, stLocal.wSecond, stLocal.wMilliseconds);
 
-    if( S_OK == dwRet )
+    if (S_OK == dwRet)
         return TRUE;
-    else return FALSE;
+    else
+        return FALSE;
 }
 
-int printfiletime(char* file){
-   HANDLE hFile;
+int printfiletime(char *file)
+{
+    HANDLE hFile;
     TCHAR szBuf[MAX_PATH];
 
     hFile = CreateFile(file, GENERIC_READ, FILE_SHARE_READ, NULL,
-        OPEN_EXISTING, 0, NULL);
+                       OPEN_EXISTING, 0, NULL);
 
-    if(hFile == INVALID_HANDLE_VALUE)
+    if (hFile == INVALID_HANDLE_VALUE)
     {
         printf("CreateFile failed with %d\n", GetLastError());
         return 0;
     }
-    if(GetLastWriteTime( hFile, szBuf, MAX_PATH ))
+    if (GetLastWriteTime(hFile, szBuf, MAX_PATH))
         _tprintf(TEXT("Last write time is: %s\n"), szBuf);
-        
-    CloseHandle(hFile);    
+
+    CloseHandle(hFile);
 
     return 0;
 }
 
 #define BUFSIZE 1024
-#define MD5LEN  16
+#define MD5LEN 16
 
-DWORD md5(char* filename)
+DWORD md5(char *filename)
 {
     DWORD dwStatus = 0;
     BOOL bResult = FALSE;
@@ -84,30 +85,30 @@ DWORD md5(char* filename)
     // Logic to check usage goes here.
 
     hFile = CreateFile(filename,
-        GENERIC_READ,
-        FILE_SHARE_READ,
-        NULL,
-        OPEN_EXISTING,
-        FILE_FLAG_SEQUENTIAL_SCAN,
-        NULL);
+                       GENERIC_READ,
+                       FILE_SHARE_READ,
+                       NULL,
+                       OPEN_EXISTING,
+                       FILE_FLAG_SEQUENTIAL_SCAN,
+                       NULL);
 
     if (INVALID_HANDLE_VALUE == hFile)
     {
         dwStatus = GetLastError();
-        printf("Error opening file %s\nError: %d\n", filename, 
-            dwStatus); 
+        printf("Error opening file %s\nError: %d\n", filename,
+               dwStatus);
         return dwStatus;
     }
 
     // Get handle to the crypto provider
     if (!CryptAcquireContext(&hProv,
-        NULL,
-        NULL,
-        PROV_RSA_FULL,
-        CRYPT_VERIFYCONTEXT))
+                             NULL,
+                             NULL,
+                             PROV_RSA_FULL,
+                             CRYPT_VERIFYCONTEXT))
     {
         dwStatus = GetLastError();
-        printf("CryptAcquireContext failed: %d\n", dwStatus); 
+        printf("CryptAcquireContext failed: %d\n", dwStatus);
         CloseHandle(hFile);
         return dwStatus;
     }
@@ -115,14 +116,14 @@ DWORD md5(char* filename)
     if (!CryptCreateHash(hProv, CALG_MD5, 0, 0, &hHash))
     {
         dwStatus = GetLastError();
-        printf("CryptAcquireContext failed: %d\n", dwStatus); 
+        printf("CryptAcquireContext failed: %d\n", dwStatus);
         CloseHandle(hFile);
         CryptReleaseContext(hProv, 0);
         return dwStatus;
     }
 
-    while (bResult = ReadFile(hFile, rgbFile, BUFSIZE, 
-        &cbRead, NULL))
+    while (bResult = ReadFile(hFile, rgbFile, BUFSIZE,
+                              &cbRead, NULL))
     {
         if (0 == cbRead)
         {
@@ -132,7 +133,7 @@ DWORD md5(char* filename)
         if (!CryptHashData(hHash, rgbFile, cbRead, 0))
         {
             dwStatus = GetLastError();
-            printf("CryptHashData failed: %d\n", dwStatus); 
+            printf("CryptHashData failed: %d\n", dwStatus);
             CryptReleaseContext(hProv, 0);
             CryptDestroyHash(hHash);
             CloseHandle(hFile);
@@ -143,7 +144,7 @@ DWORD md5(char* filename)
     if (!bResult)
     {
         dwStatus = GetLastError();
-        printf("ReadFile failed: %d\n", dwStatus); 
+        printf("ReadFile failed: %d\n", dwStatus);
         CryptReleaseContext(hProv, 0);
         CryptDestroyHash(hHash);
         CloseHandle(hFile);
@@ -155,133 +156,128 @@ DWORD md5(char* filename)
     {
         printf("MD5 hash of file %s is: ", filename);
         DWORD i;
-        for ( i = 0; i < cbHash; i++)
+        for (i = 0; i < cbHash; i++)
         {
             printf("%c%c", rgbDigits[rgbHash[i] >> 4],
-                rgbDigits[rgbHash[i] & 0xf]);
+                   rgbDigits[rgbHash[i] & 0xf]);
         }
         printf("\n");
     }
     else
     {
         dwStatus = GetLastError();
-        printf("CryptGetHashParam failed: %d\n", dwStatus); 
+        printf("CryptGetHashParam failed: %d\n", dwStatus);
     }
 
     CryptDestroyHash(hHash);
     CryptReleaseContext(hProv, 0);
     CloseHandle(hFile);
 
-    return dwStatus; 
-}   
-
-
-int printdir(char* folder)
-{
-   WIN32_FIND_DATA ffd;
-   LARGE_INTEGER filesize;
-   TCHAR szDir[MAX_PATH];
-   TCHAR szSubDir[MAX_PATH];
-   size_t length_of_arg;
-   HANDLE hFind = INVALID_HANDLE_VALUE;
-   DWORD dwError=0;
-   
-  
-
-   // Check that the input path plus 3 is not longer than MAX_PATH.
-   // Three characters are for the "\*" plus NULL appended below.
-
-   StringCchLength(folder, MAX_PATH, &length_of_arg);
-
-   if (length_of_arg > (MAX_PATH - 3))
-   {
-      _tprintf(TEXT("\nDirectory path is too long.\n"));
-      return (-1);
-   }
-
-   _tprintf(TEXT("\nTarget directory is %s\n\n"), folder);
-
-   // Prepare string for use with FindFile functions.  First, copy the
-   // string to a buffer, then append '\*' to the directory name.
-
-   StringCchCopy(szDir, MAX_PATH, folder);
-   StringCchCat(szDir, MAX_PATH, TEXT("\\*"));
-
-   // Find the first file in the directory.
-
-   hFind = FindFirstFile(szDir, &ffd);
-
-   if (INVALID_HANDLE_VALUE == hFind) 
-   {
-     _tprintf(TEXT("FindFirstFile"));
-      return dwError;
-   } 
-   
-   // List all the files in the directory with some info about them.
-
-   do
-   {
-      if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
-      {
-         _tprintf(TEXT("  %s   <DIR>\n"), ffd.cFileName);
-         if(strcmp(".",ffd.cFileName)&&strcmp("..",ffd.cFileName)){
-            StringCchCopy(szSubDir, MAX_PATH, folder);
- StringCchCat(szSubDir, MAX_PATH, TEXT("\\"));
-   StringCchCat(szSubDir, MAX_PATH, ffd.cFileName);
-         printdir(szSubDir);
-     }
-      }
-      else
-      {
-         filesize.LowPart = ffd.nFileSizeLow;
-         filesize.HighPart = ffd.nFileSizeHigh;
-
-
-         _tprintf(TEXT("  %s   %ld bytes\n"), ffd.cFileName, filesize.QuadPart);
-
-    StringCchCopy(szSubDir, MAX_PATH, folder);
- StringCchCat(szSubDir, MAX_PATH, TEXT("\\"));
-   StringCchCat(szSubDir, MAX_PATH, ffd.cFileName);
-         printfiletime(szSubDir);
-
-         md5(szSubDir);
-      }
-   }
-   while (FindNextFile(hFind, &ffd) != 0);
- 
-   dwError = GetLastError();
-   if (dwError != ERROR_NO_MORE_FILES) 
-   {
-   _tprintf(TEXT("FindFirstFile"));
-   }
-
-   FindClose(hFind);
-   return 0;
+    return dwStatus;
 }
 
+int printdir(char *folder)
+{
+    WIN32_FIND_DATA ffd;
+    LARGE_INTEGER filesize;
+    TCHAR szDir[MAX_PATH];
+    TCHAR szSubDir[MAX_PATH];
+    size_t length_of_arg;
+    HANDLE hFind = INVALID_HANDLE_VALUE;
+    DWORD dwError = 0;
+
+    // Check that the input path plus 3 is not longer than MAX_PATH.
+    // Three characters are for the "\*" plus NULL appended below.
+
+    StringCchLength(folder, MAX_PATH, &length_of_arg);
+
+    if (length_of_arg > (MAX_PATH - 3))
+    {
+        _tprintf(TEXT("\nDirectory path is too long.\n"));
+        return (-1);
+    }
+
+    _tprintf(TEXT("\nTarget directory is %s\n\n"), folder);
+
+    // Prepare string for use with FindFile functions.  First, copy the
+    // string to a buffer, then append '\*' to the directory name.
+
+    StringCchCopy(szDir, MAX_PATH, folder);
+    StringCchCat(szDir, MAX_PATH, TEXT("\\*"));
+
+    // Find the first file in the directory.
+
+    hFind = FindFirstFile(szDir, &ffd);
+
+    if (INVALID_HANDLE_VALUE == hFind)
+    {
+        _tprintf(TEXT("FindFirstFile"));
+        return dwError;
+    }
+
+    // List all the files in the directory with some info about them.
+
+    do
+    {
+        if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+        {
+            _tprintf(TEXT("  %s   <DIR>\n"), ffd.cFileName);
+            if (strcmp(".", ffd.cFileName) && strcmp("..", ffd.cFileName))
+            {
+                StringCchCopy(szSubDir, MAX_PATH, folder);
+                StringCchCat(szSubDir, MAX_PATH, TEXT("\\"));
+                StringCchCat(szSubDir, MAX_PATH, ffd.cFileName);
+                printdir(szSubDir);
+            }
+        }
+        else
+        {
+            filesize.LowPart = ffd.nFileSizeLow;
+            filesize.HighPart = ffd.nFileSizeHigh;
+
+            _tprintf(TEXT("  %s   %ld bytes\n"), ffd.cFileName, filesize.QuadPart);
+
+            StringCchCopy(szSubDir, MAX_PATH, folder);
+            StringCchCat(szSubDir, MAX_PATH, TEXT("\\"));
+            StringCchCat(szSubDir, MAX_PATH, ffd.cFileName);
+            printfiletime(szSubDir);
+
+            md5(szSubDir);
+        }
+    } while (FindNextFile(hFind, &ffd) != 0);
+
+    dwError = GetLastError();
+    if (dwError != ERROR_NO_MORE_FILES)
+    {
+        _tprintf(TEXT("FindFirstFile"));
+    }
+
+    FindClose(hFind);
+    return 0;
+}
 
 int watch(int argc, TCHAR *argv[])
 {
     HANDLE hFile;
     TCHAR szBuf[MAX_PATH];
 
-    if( argc != 2 )
+    if (argc != 2)
     {
         printf("This sample takes a file name as a parameter\n");
         return 0;
     }
     hFile = CreateFile(argv[1], GENERIC_READ, FILE_SHARE_READ, NULL,
-        OPEN_EXISTING, 0, NULL);
+                       OPEN_EXISTING, 0, NULL);
 
-    if(hFile == INVALID_HANDLE_VALUE)
+    if (hFile == INVALID_HANDLE_VALUE)
     {
         printf("CreateFile failed with %d\n", GetLastError());
         return 0;
     }
-    if(GetLastWriteTime( hFile, szBuf, MAX_PATH ))
+    if (GetLastWriteTime(hFile, szBuf, MAX_PATH))
         _tprintf(TEXT("Last write time is: %s\n"), szBuf);
-        
-    CloseHandle(hFile);    
+
+    CloseHandle(hFile);
 
     printdir("src");
 }
@@ -292,24 +288,23 @@ int watch(int argc, TCHAR *argv[])
 
 // #include <boost/filesystem/operations.hpp>
 
-
 // const char *filename = "src/main.c";
- 
+
 // int main() {
 //   struct stat foo;
 //   struct timeval new_times[2];
- 
+
 //   if (stat(filename, &foo) < 0)
 //     err(1, "%s", filename);
- 
-//    keep atime unchanged 
+
+//    keep atime unchanged
 //   TIMESPEC_TO_TIMEVAL(&new_times[0], &foo.st_atim);
- 
+
 //   /* set mtime to current time */
 //   gettimeofday(&new_times[1], NULL);
- 
+
 //   if (utimes(filename, new_times) < 0)
 //     err(1, "%s", filename);
- 
+
 //   return 0;
 // }
